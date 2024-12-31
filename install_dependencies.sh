d7b9b307d536aec3f683d4e209dcb9bd662fd294
nikunj-gupta <ng2531@nyu.edu>
2024-12-31 02:07:59 -0800

    updated dependencies for pogema, torch_geometric, and smacv2

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Install PyTorch and Python Packages
 
-# conda create -n pymarl python=3.8 -y
-# conda activate pymarl
+# conda create -n tgatmarl python=3.11 -y
+# conda activate tgatmarl
 
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
-pip install protobuf==3.19.5 sacred==0.7.5 numpy scipy gym==0.11 matplotlib seaborn \
-    pyyaml==5.3.1 pygame pytest probscale imageio snakeviz tensorboard-logger
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y 
+
+pip install protobuf==3.20.* sacred numpy scipy gym==0.11 matplotlib seaborn \
+    pyyaml pygame pytest probscale imageio snakeviz tensorboard-logger pymongo setproctitle 
 pip install git+https://github.com/oxwhirl/smac.git@26f4c4e4d1ebeaf42ecc2d0af32fac0774ccc678
+pip install git+https://github.com/oxwhirl/smacv2.git 
+
+pip install torch_geometric
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.1+cu121.html 
+pip install pogema pogema-toolbox 
